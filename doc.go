fe378bf7f0052f75ceca16e550a724664dfd9bbf
Emmanuel Ay <emmanuel@block21.com>
2022-10-02 16:30:35 +0200

    Package description

---
+++ doc.go
@@ -0,0 +1,10 @@
+// Anonymize is a small and simple package for anonymizing names, e-mails and domains.
+//
+// Pass strings to anonymize to the provided public methods: 'Name', 'Email' or 'Domain'.
+// Strings are by default anonymized using the asterix character, provide your own anonymization
+// character by using the 'WithCustomRune' methods.
+//
+// Copyright 2022 Emmanuel Ay. All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+package anonymize
